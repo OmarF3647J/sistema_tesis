55590a231d4d078e57cf5fc588ad1a894011e1d1
OmarF3647J <omarfiscalpolito@hotmail.com>
2026-04-18 15:29:44 -0600

    2

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -1,5 +1,8 @@
 <template>
-  <label class="block text-sm text-gray-700">
+  <label
+    v-bind="$attrs"
+    class="block text-sm text-gray-700"
+  >
     <span v-if="value">{{ value }}</span>
     <span v-else>
       <slot />
