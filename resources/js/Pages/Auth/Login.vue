ef15918ed77ca3560252f94abb74846ff1433d27
OmarF3647J <omarfiscalpolito@hotmail.com>
2026-04-18 15:18:35 -0600

    imput x2

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -48,7 +48,7 @@ const submit = () => {
 					<form @submit.prevent="submit">
 						<div class="mt-4">
 							<InputLabel for="email" value="Usuario" />
-							<TextInput id="name" type="name" class="block w-full mt-1" v-model="form.name" required autofocus autocomplete="username" />
+							<TextInput id="email" type="email" class="block w-full mt-1" v-model="form.name" required autofocus autocomplete="username" />
 							<InputError class="mt-2" :message="form.errors.name" />
 						</div>
 						
