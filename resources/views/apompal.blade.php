9114e5cd4136e545f9d7acd82a109c83c8bd1713
OmarF3647J <omarfiscalpolito@hotmail.com>
2026-05-18 23:56:57 -0600

    18

--- resources/views/apompal.blade.php
+++ resources/views/apompal.blade.php
@@ -290,11 +290,25 @@
           <div style="text-align: center;">
             <h3>{{ $agencia->nomguiatur }}</h3>
           </div>
+
           <h4>Responsable: <span style="font-weight: 300;">{{ $agencia->respguiatur }}</span> </h4>
           <h4>Número: <span style="font-weight: 300;">{{ $agencia->numguiatur }}</span></h4>
           <h4>Correo: <span style="font-weight: 300;">{{ $agencia->correoguiatur }}</span></h4>   
           <h4>Actividades que realizan <span style="font-weight: 300;">{{ $agencia->actguiatur }}</span></h4>
-        
+      
+          <br>
+        <!-- Contenedor de iconos -->
+        <div class="btn-redes-sociales">
+          <a href="https://www.facebook.com/profile.php?id=100064519459589" target="_blank" class="cat">
+            <img src="{{ asset('images/favicon/facebook_circular.png') }}" alt="Facebook" loading="lazy">
+          </a>
+          <a href="https://www.instagram.com/degraaftraveltours?igsh=MWg3YTFsbjdmemlveQ%3D%3D&utm_source=qr&fbclid=IwY2xjawN6R-tleHRuA2FlbQIxMABicmlkETFBd3B5Q2JCN0FLY2pRdTlEc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHmAPC4UW07qkGHi7caS05Rp0OmKZiZQnAcT9zNxRqhhVkSfC5wmPF3CQ3sl8_aem_XNLgHFftYwkMk6AeiRMA4w" target="_blank" class="cat">
+            <img src="{{ asset('images/favicon/ig.png') }}" alt="Instagram" loading="lazy">
+          </a>
+          <a href="https://www.tiktok.com/@degraaf.travel.to?_r=1&_t=ZS-91B3jHc6KSg&fbclid=IwY2xjawN6SCdleHRuA2FlbQIxMABicmlkETFBd3B5Q2JCN0FLY2pRdTlEc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHk1QmcF-zDkXnHlBYhg7ieo2ktE70HKERQPlEtUvfCu8ZMd9TrU9y6daJJwT_aem_aWHbbPqZIeyRJD-mFh4PXw" target="_blank" class="cat">
+            <img src="{{ asset('images/favicon/tiktok-circular.png') }}" alt="Tiktok" loading="lazy">
+          </a>
+        </div>
 
         </div>
         @endforeach
