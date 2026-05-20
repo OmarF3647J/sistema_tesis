ab5dbcab58eb6f1c6ca15a96ff7eecb6fe75988b
CMillan21 <christianmanuelmillanpolito9@gmail.com>
2026-05-19 23:38:37 -0600

    padding 3

--- resources/js/Layouts/Navigation.vue
+++ resources/js/Layouts/Navigation.vue
@@ -26,15 +26,15 @@
         </li>
       -->
 
-        <li class="relative px-4 py-8">
+        <li class="relative px-8 py-4">
           <NavLink 
             :href="route('dashboard')" 
             :active="route().current('dashboard')"
             class="flex items-center w-full px-4 py-3 rounded-lg font-medium text-sm transition-colors duration-150"
           >
             <template #icon>
-              <svg class="w-5 h-5 mr-3" aria-hidden="true" fill="none" stroke-linecap="round"
-                  stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+              <svg class="w-5 h-5 mr-3" aria-hidden="true" fill="none" stroke-linecap="rounded-lg"
+                  stroke-linejoin="rounded-lg" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
               </svg>
             </template>
